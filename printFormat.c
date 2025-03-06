eca7986e345c30582c0cd6fa0c53294ba7057f7a
Yuvansh Pratap Singh <yuvansh2255@gmail.com>
2025-03-06 23:33:00 +0530

    Add file

---
+++ printFormat.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int x = printf("KG");
+    printf("%d ",x);
+
+	int a = printf("Hello");
+	printf("%d ",a);
+
+    printf("%d %d",printf(" Hello "), printf("World "));
+
+    return 0;
+}
---
+++ variable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int a = 32768;
+    // It's range from -32768 to 32767
+    unsigned int b = -1;
+    // It's range from 0 to 65535
+    char c = 131;
+    // It's range from -128 to 127
+
+    printf("%d ",a);
+    printf("%u ",b);
+    printf("%d ",c);
+
+    // 16-bit
+}
